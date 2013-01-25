nvie/new_workers

commit 34893c4a3d13157fa0f5722c09a08b52f98a7268

Code cleanup related to signal handlers.

base_worker.py
@@ -2,6 +2,7 @@ import random
 import time
 import datetime
 import signal
+from helpers import install_signal_handlers
 
 
 def slow_fib(n):
@@ -42,7 +43,7 @@ class FakeWorkMethodMixin(object):
 class BaseWorker(FakeWorkMethodMixin):
 
     def work(self):
-        self.install_signal_handlers()
+        install_signal_handlers()
 
         while True:
             try:
@@ -59,14 +60,6 @@ class BaseWorker(FakeWorkMethodMixin):
             print 'Children killed. You murderer.'
 
         print 'Shut down'
-
-    #def disable_interrupts(self):
-    #    signal.signal(signal.SIGINT, signal.SIG_IGN)
-    #    signal.signal(signal.SIGTERM, signal.SIG_IGN)
-
-    def install_signal_handlers(self):
-        signal.signal(signal.SIGINT, signal.default_int_handler)
-        signal.signal(signal.SIGTERM, signal.default_int_handler)
 
     def get_ident(self):
         raise NotImplementedError('Implement this in a subclass.')

forking_worker.py
@@ -5,6 +5,7 @@ import random
 import errno
 from multiprocessing import Semaphore, Array
 from base_worker import BaseWorker
+from helpers import Interruptable
 
 
 def waitpid(pid):
@@ -80,13 +81,11 @@ class ForkingWorker(BaseWorker):
 
     def spawn_child(self):
         """Forks and executes the job."""
-        self.install_signal_handlers()
-
         # Responsible for the blocking, may be interrupted by SIGINT or
         # SIGTERM, the worker's main loop will catch it
-        self._semaphore.acquire()
+        with Interruptable():
+            self._semaphore.acquire()
 
-        self._disable_interrupts()
         self._fork()
 
     def wait_for_children(self):
@@ -127,11 +126,7 @@ class ForkingWorker(BaseWorker):
 
     ##
     # Helper methods (specific to forking workers)
-    def _disable_interrupts(self):  # noqa
-        signal.signal(signal.SIGINT, signal.SIG_IGN)
-        signal.signal(signal.SIGTERM, signal.SIG_IGN)
-
-    def _fork(self):
+    def _fork(self):  # noqa
         slot = self._claim_slot()
 
         # The usual hardcore forking action

helpers.py
@@ -1,0 +1,19 @@
+import signal
+
+
+def install_signal_handlers():
+    signal.signal(signal.SIGINT, signal.default_int_handler)
+    signal.signal(signal.SIGTERM, signal.default_int_handler)
+
+
+def disable_interrupts():
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
+    signal.signal(signal.SIGTERM, signal.SIG_IGN)
+
+
+class Interruptable(object):
+    def __enter__(self):
+        install_signal_handlers()
+
+    def __exit__(self, type, value, traceback):
+        disable_interrupts()

commit 983ed582f3d88f3e0449352313bbcd30e389a939

Fix: 34893c4a3d13157fa0f5722c09a08b52f98a7268 broke gevent worker. This restores it.

base_worker.py
@@ -1,7 +1,6 @@
 import random
 import time
 import datetime
-import signal
 from helpers import install_signal_handlers
 
 
@@ -43,7 +42,7 @@ class FakeWorkMethodMixin(object):
 class BaseWorker(FakeWorkMethodMixin):
 
     def work(self):
-        install_signal_handlers()
+        self.install_signal_handlers()
 
         while True:
             try:
@@ -60,6 +59,9 @@ class BaseWorker(FakeWorkMethodMixin):
             print 'Children killed. You murderer.'
 
         print 'Shut down'
+
+    def install_signal_handlers(self):
+        install_signal_handlers()
 
     def get_ident(self):
         raise NotImplementedError('Implement this in a subclass.')