f74b87f95857c788fcfa1f6e478f5334b2eb6f85
Vincent Driessen <vincent@3rdcloud.com>
2013-01-25 22:48:01 +0100

    Reorganize methods on GeventWorker, based on whether they are overridden from BaseWorker.

--- gevent_worker.py
+++ gevent_worker.py
@@ -27,6 +27,8 @@ def safe_wrap(func):
 
 class GeventWorker(BaseWorker):
 
+    ##
+    # Overridden from BaseWorker
     def __init__(self, num_processes=1):
         self._pool = gevent.pool.Pool(num_processes)
         self._busy_children = {}
@@ -40,28 +42,12 @@ class GeventWorker(BaseWorker):
     def get_ident(self):
         return id(gevent.getcurrent())
 
-    def unregister_child(self, child):
-        print '==> Unregistering {}'.format(id(child))
-        del self._busy_children[child]
-
     def spawn_child(self):
         """Forks and executes the job."""
         busy_flag = Event()
-        child_greenlet = self._pool.spawn(self.main_child, busy_flag)
+        child_greenlet = self._pool.spawn(self._main_child, busy_flag)
         self._busy_children[child_greenlet] = busy_flag
-        child_greenlet.link(self.unregister_child)
-
-    def main_child(self, busy_flag):
-        #safe_wrap(self.fake))
-        busy_flag.clear()
-        time.sleep(random.random() * 4)  # TODO: Fake BLPOP behaviour
-        busy_flag.set()
-
-        time.sleep(0)  # TODO: Required to avoid "blocking" by CPU-bound jobs
-        try:
-            self.fake()
-        finally:
-            busy_flag.clear()
+        child_greenlet.link(self._unregister_child)
 
     def terminate_idle_children(self):
         print 'Find all children that are in idle state (waiting for work)...'
@@ -82,6 +68,25 @@ class GeventWorker(BaseWorker):
         self._pool.kill()
         print 'MWHUAHAHAHAHA!'
         self.wait_for_children()
+
+
+    ##
+    # Helper methods (specific to gevent workers)
+    def _unregister_child(self, child):
+        print '==> Unregistering {}'.format(id(child))
+        del self._busy_children[child]
+
+    def _main_child(self, busy_flag):
+        #safe_wrap(self.fake))
+        busy_flag.clear()
+        time.sleep(random.random() * 4)  # TODO: Fake BLPOP behaviour
+        busy_flag.set()
+
+        time.sleep(0)  # TODO: Required to avoid "blocking" by CPU-bound jobs
+        try:
+            self.fake()
+        finally:
+            busy_flag.clear()
 
 
 if __name__ == '__main__':
